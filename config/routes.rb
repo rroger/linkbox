ca920a9361072139eba4dc03033b5672ed1cc650
Roger Rüttimann <ror@panter.ch>
2018-09-05 17:44:45 +0200

    add fast_jsonapi and fix linting once more

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   root to: 'landing#index'
+
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :topics, only: %i[index show create destroy update]
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
 # frozen_string_literal: true
+15.times { FactoryBot.create(:topic) }
