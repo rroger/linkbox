416f2a12483a66425eca2bbec9edf19cfc5cdb66
Roger Rüttimann <ror@panter.ch>
2018-09-10 17:27:31 +0200

    improve topics form. add bevhaviour and style

--- app/javascript/components/topics.vue
+++ app/javascript/components/topics.vue
@@ -4,11 +4,8 @@
       <div class="mr-auto col-md-4 ml-auto">
         <div class="topics-header">
           <div class="row">
-            <div class="col-md-9">
-              <button class="topics-add btn btn-outline-light" v-on:click="showForm = !showForm">+ Add Topic</button>
-            </div>
-            <div class="col-md-3">
-              <span class="topics-counter">({{ topics.length }})</span>
+            <div class="col-md-12">
+              <button class="topics-add btn btn-outline-light" @click="toggleFormVisibility">+ Add Topic</button>
             </div>
           </div>
           <div class="row">
@@ -21,7 +18,8 @@
                 </div>
                 <div class="row">
                   <div class=col-md-12>
-                    <button type="button" class="btn btn-outline-primary">Delete</button>
+                    <button v-show="editTopicId" type="button" class="btn btn-outline-primary" @click="deleteTopic()">Delete</button>
+                    <button v-show="!editTopicId" type="button" class="btn btn-outline-primary" @click="showForm = false">Cancel</button>
                     <button type="submit" class="btn btn-primary save-button">Save</button>
                   </div>
                 </div>
@@ -30,8 +28,13 @@
           </div>
         </div>
         <ul>
-          <li v-bind:key="topic.name" v-for="topic in topics">
-            {{ topic.name }}
+          <li>Topics <span class="topics-counter">({{ topics.length }})</span></li>
+          <li>&nbsp;</li>
+          <li class="topics-row" v-bind:key="topic.name" v-for="topic in topics">
+            <span>
+              {{ topic.name }}
+            </span>
+            <button type="button" class="btn btn-outline-light edit-button" @click="edit(topic)">EDIT</button>
           </li>
         </ul>
       </div>
@@ -43,21 +46,36 @@
 import _ from 'lodash';
 export default {
   name: 'TopicsComponent',
+
   data() {
     return {
       topics: [{ name: 'Test Topic' }],
       showForm: false,
       newTopicName: null,
+      editTopicId: null,
+      baseUrl: 'http://localhost:3000/api/v1/topics',
     };
   },
   created() {
     this.fetchTopics();
   },
   methods: {
+    toggleFormVisibility(){
+      this.showForm = !this.showForm;
+      if (!this.showForm) {
+        this.clearForm();
+      }
+    },
+    clearForm() {
+      this.newTopicName = null;
+      this.editTopicId = null;
+    },
     fetchTopics() {
-      this.$http.get('http://localhost:3000/api/v1/topics').then(response => {
+      this.$http.get(this.baseUrl).then(response => {
         const topics = _.map(response.body['data'], (raw) => {
-          return raw.attributes;
+          const topic = raw.attributes;
+          topic.id = raw.id;
+          return topic;
         });
         this.topics = topics;
       });
@@ -71,9 +89,29 @@ export default {
           }
         }
       };
-      this.$http.post('http://localhost:3000/api/v1/topics', params).then(() => {
+      if (this.editTopicId) {
+        params.data.id = this.editTopicId;
+        this.$http.put(`${this.baseUrl}/${this.editTopicId}`, params).then(() => {
+          this.fetchTopics();
+          this.clearForm();
+          this.showForm = false;
+        });
+      } else {
+        this.$http.post(this.baseUrl, params).then(() => {
+          this.fetchTopics();
+          this.newTopicName = null;
+        });
+      }
+    },
+    edit(topic) {
+      this.editTopicId = topic.id;
+      this.newTopicName = topic.name;
+      this.showForm = true;
+    },
+    deleteTopic() {
+      this.$http.delete(`${this.baseUrl}/${this.editTopicId}`).then(() => {
         this.fetchTopics();
-        this.newTopicName = null;
+        this.clearForm();
         this.showForm = false;
       });
     }
@@ -95,8 +133,8 @@ export default {
 
   .topics-counter {
     font-size: 0.7em !important;
-    float: right;
     color: $gray;
+    vertical-align: super;
   }
 
   .topics-form {
@@ -128,6 +166,25 @@ export default {
     list-style: none;
     padding-left: 10px;
     margin-top: 15px;
+  }
+
+
+  .topics-row {
+    .edit-button {
+      color: transparent;
+      border-color: transparent;
+      line-height: 0.6rem;
+      font-size: 0.6rem;
+      float: right;
+    }
+  }
+
+  .topics-row:hover {
+    .edit-button {
+      color: $dark-gray;
+      border-color: transparent;
+    }
+    background-color: $light-gray;
   }
 }
 </style>
