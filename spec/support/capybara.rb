4bb7513c39f2ab651da4a1bc7844e8c2f0a50ca6
Roger Rüttimann <ror@panter.ch>
2018-09-10 22:26:42 +0200

    and another try, this time really loading the capybara config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,12 +5,11 @@ require 'capybara-screenshot/rspec'
 Capybara.register_driver :firefox_headless do |app|
   options = ::Selenium::WebDriver::Firefox::Options.new
   options.args << '--headless'
-  options.add_preference 'dom.file.createInChild', true
   Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
 end
-# Capybara.register_driver :firefox do |app|
-#   Capybara::Selenium::Driver.new(app, browser: :firefox)
-# end
+Capybara.register_driver :firefox do |app|
+  Capybara::Selenium::Driver.new(app, browser: :firefox)
+end
 Capybara.javascript_driver = :firefox_headless
 # From https://github.com/mattheworiordan/capybara-screenshot/issues/84#issuecomment-41219326
 Capybara::Screenshot.register_driver(:firefox_headless) do |driver, path|
