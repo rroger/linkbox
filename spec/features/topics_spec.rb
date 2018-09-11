0a18190435c561f44e1f6828232c415ac19dd515
Roger Rüttimann <ror@panter.ch>
2018-09-11 12:28:35 +0200

    use fixed Names in topic e2e test

--- spec/features/topics_spec.rb
+++ spec/features/topics_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Topics page', :js do
-  let!(:topics) { create_list(:topic, 5) }
+  let!(:topics) { [create(:topic, name: 'AI'), create(:topic, name: 'Style')] }
 
   before { visit root_path }
   it 'can list topics' do
@@ -18,26 +18,24 @@ RSpec.describe 'Topics page', :js do
     find(:css, '.save-button').click
 
     expect(page).to have_content 'TopicSoNew'
-    expect(Topic.count).to be 6
+    expect(Topic.count).to be 3
   end
 
   it 'can edit a topic' do
     find_all(:css, '.edit-button').first.click
-    old_topic_name = find_field('topic-name').value
     fill_in 'topic-name', with: 'TopicSoEdited'
     find(:css, '.save-button').click
 
-    expect(Topic.count).to be 5
+    expect(Topic.count).to be 2
     expect(page).to have_content 'TopicSoEdited'
-    expect(page).to_not have_content old_topic_name
+    expect(page).to_not have_content 'AI'
   end
 
   it 'can delete a topic' do
     find_all(:css, '.edit-button').first.click
-    old_topic_name = find_field('topic-name').value
     find(:css, "button[data-test='delete-button']").click
 
-    expect(Topic.count).to be 4
-    expect(page).to_not have_content old_topic_name
+    expect(Topic.count).to be 1
+    expect(page).to_not have_content 'AI'
   end
 end
