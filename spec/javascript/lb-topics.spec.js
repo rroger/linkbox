be9be2c08a38166365bf82a33390efd9c0b07f1e
Roger Rüttimann <ror@panter.ch>
2018-09-15 18:13:54 +0200

    not working: try to mock this.$http in topics, so component can be loaded

--- spec/javascript/lb-topics.spec.js
+++ spec/javascript/lb-topics.spec.js
@@ -1,3 +1,4 @@
+
 import { mount, shallowMount } from "@vue/test-utils"
 import TopicsComponent from "../../app/javascript/components/lb-topics.vue"
 // import ConfirmationComponent from "../../app/javascript/components/lb-confirmation.vue"
@@ -9,19 +10,19 @@ import TopicsComponent from "../../app/javascript/components/lb-topics.vue"
 // jest.mock('TopicsComponent.$http.get', () => ({
 //   get: jest.fn(() => )
 // }));
-TopicsComponent.$http = {
+const http = {
   get: function(args) {
     return Promise.resolve({"data":[{"id":"55","type":"topic","attributes":{"name":"One mores"}},{"id":"52","type":"topic","attributes":{"name":"So goodOk"}},{"id":"111","type":"topic","attributes":{"name":"Totaly New Topic"}},{"id":"112","type":"topic","attributes":{"name":"Typography"}}]});
   }
 }
 
-console.log('this httpget: ', TopicsComponent.$http.get(args));
+// console.log('this httpget: ', TopicsComponent.$http.get('url',''));
 
 describe('TopicsComponent', () => {
   // Now mount the component and you have the wrapper
   console.log('debug: ', TopicsComponent);
-  const wrapper = shallowMount(TopicsComponent);
-
+  const wrapper = mount(TopicsComponent);
+  wrapper.setValue(http);
   it('renders the correct markup', () => {
     expect(wrapper.html()).toContain('<span class="count">0</span>');
   })
