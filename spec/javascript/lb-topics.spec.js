0d0cf4c7f48eae85d4f905d1a1ef058aa7d97a62
Roger Rüttimann <ror@panter.ch>
2018-09-15 23:36:28 +0200

    fix linting and add yarn tets to bin/check

--- spec/javascript/lb-topics.spec.js
+++ spec/javascript/lb-topics.spec.js
@@ -1,28 +1,28 @@
 import Vue from 'vue/dist/vue.js'
-import { mount, shallowMount, createLocalVue } from "@vue/test-utils"
-import LbTopics from "../../app/javascript/components/lb-topics.vue"
-import LbConfirmation from "../../app/javascript/components/lb-confirmation.vue"
+import { shallowMount } from '@vue/test-utils'
+import LbTopics from '../../app/javascript/components/lb-topics.vue'
+import LbConfirmation from '../../app/javascript/components/lb-confirmation.vue'
 
 
 const $httpSuccess = {
   get(){
     return Promise.resolve({
       body: {
-        "data": [
-          {"id":"55","type":"topic","attributes":{"name":"One mores"}},
-          {"id":"52","type":"topic","attributes":{"name":"So goodOk"}},
-          {"id":"111","type":"topic","attributes":{"name":"Totaly New Topic"}},
-          {"id":"112","type":"topic","attributes":{"name":"Typography"}}
+        'data': [
+          {'id':'55','type':'topic','attributes':{'name':'One mores'}},
+          {'id':'52','type':'topic','attributes':{'name':'So goodOk'}},
+          {'id':'111','type':'topic','attributes':{'name':'Totaly New Topic'}},
+          {'id':'112','type':'topic','attributes':{'name':'Typography'}}
         ]
       }
-  });
+    })
   }
-};
-const $httpFail = {
-  get(){
-    return Promise.reject({body: { message: "internal server error" }});
-  }
-};
+}
+// const $httpFail = {
+//   get(){
+//     return Promise.reject({body: { message: 'internal server error' }})
+//   }
+// }
 
 describe('TopicsComponent', () => {
 
@@ -34,22 +34,22 @@ describe('TopicsComponent', () => {
         },
         $http: $httpSuccess
       }
-    });
-    expect(wrapper.html()).toContain('<li>Topics');
+    })
+    expect(wrapper.html()).toContain('<li>Topics')
   })
 
-  it('loads and displays topics', async (done) => {
+  it('loads and displays topics', (done) => {
     const wrapper = shallowMount(LbTopics,  {
       mocks: {
         $http: $httpSuccess
       }
-    });
+    })
     Vue.nextTick(() => {
-      expect(wrapper.html()).toContain('One mores');
-      expect(wrapper.html()).toContain('So goodOk');
-      expect(wrapper.html()).toContain('Totaly New Topic');
-      expect(wrapper.html()).toContain('Typography');
-      done();
+      expect(wrapper.html()).toContain('One mores')
+      expect(wrapper.html()).toContain('So goodOk')
+      expect(wrapper.html()).toContain('Totaly New Topic')
+      expect(wrapper.html()).toContain('Typography')
+      done()
     })
   })
-});
+})
