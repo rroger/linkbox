cf47810bd6664d0c6fe91b54ebeb53ba58fef42a
Roger Rüttimann <ror@panter.ch>
2018-09-19 08:22:19 +0200

    add image to nav

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -4,9 +4,8 @@
       <form class="form-inline" v-on:submit.prevent="onSubmit">
         <div class="input-group">
           <div class="input-group-prepend">
-            <span class="input-group-text" id="basic-addon1">@</span>
+            <img src="/public/images/add-24-px.svg" />
           </div>
-          <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
           <input class="btn btn-primary" type="submit" @click="sendtest()">
         </div>
       </form>
