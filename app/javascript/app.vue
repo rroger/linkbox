d1988ab7735f459e34aa138b15b6118bcdf7d1cd
Roger Rüttimann <ror@panter.ch>
2018-09-27 10:43:54 +0200

    try to add svg

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -4,7 +4,11 @@
       <form class="form-inline" v-on:submit.prevent="onSubmit">
         <div class="input-group">
           <div class="input-group-prepend">
-            <img src="/public/images/add-24-px.svg" />
+            <div class="add-button">
+              <div class="add-frame">
+                <img src="./images/add-24-px.svg" class="add-sign" />
+              </div>
+            </div>
           </div>
           <input class="btn btn-primary" type="submit" @click="sendtest()">
         </div>
@@ -30,5 +34,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  /*@import '../assets/stylesheets/landing.scss';*/
+  @import './stylesheets/variables';
+
+  .add-button {
+    width: 123px;
+    height: 30px;
+    background-color: #ffffff;
+
+    .add-frame {
+      width: 28px;
+      height: 28px;
+      background-color: $dark-gray;
+
+      .add-sign {
+        width: 24px;
+        height: 24px;
+        object-fit: contain;
+        fill: white;
+      }
+    }
+
+  }
 </style>
