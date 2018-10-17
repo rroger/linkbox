656b08a4579d032236c40c5d16f054d7e3e175a6
Roger Rüttimann <ror@panter.ch>
2018-10-17 08:48:12 +0200

    extend sleep for test on ci

--- spec/features/topics_spec.rb
+++ spec/features/topics_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe 'Topics page', :js do
     it 'can delete a topic' do
       click_button 'EDIT', match: :first
       click_button 'Delete'
-      sleep(0.1)
+      sleep(0.3)
       click_button 'Proceed'
 
       expect(Topic.count).to be 1
